0a2c6dead87d50e1f371d89a17f2e3a0388892d9
ffferncake <fernnichanun@gmail.com>
2025-06-19 11:28:31 +0900

    [test] matadata for kakao,line 등

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,38 +4,38 @@ import Image from "next/image";
 // import { useEffect } from "react";
 // import { motion, useAnimation } from "framer-motion";
 import ScrollSection from "../app/components/ScrollSection"; // Client Component
-import Head from "next/head";
+// import Head from "next/head";
 
-export const metadata = {
-  title: '지훈❤️수아 결혼식에 초대합니다',
-  description: '2025년 10월 18일 토요일 오후 1시, 서울 라마다호텔 2층 그랜드홀에서 만나요.',
-  openGraph: {
-    title: '지훈❤️수아 결혼식에 초대합니다',
-    description: '2025년 10월 18일 토요일 오후 1시, 서울 라마다호텔 2층 그랜드홀에서 만나요.',
-    url: 'https://fern-eunsang.vercel.app/',
-    siteName: '지훈❤️수아 모바일 청첩장',
-    images: [
-      {
-        url: 'https://github.com/ffferncake/mobile-wedding-app/blob/main/public/photo1.PNG', // Replace with actual image URL
-        width: 800,
-        height: 600,
-        alt: '지훈❤️수아 웨딩 사진',
-      },
-    ],
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: '지훈❤️수아 결혼식에 초대합니다',
-    description: '2025년 10월 18일 토요일 오후 1시, 서울 라마다호텔 2층 그랜드홀에서 만나요.',
-    images: ['https://github.com/ffferncake/mobile-wedding-app/blob/main/public/photo1.PNG'],
-  },
-}
+// export const metadata = {
+//   title: '지훈❤️수아 결혼식에 초대합니다',
+//   description: '2025년 10월 18일 토요일 오후 1시, 서울 라마다호텔 2층 그랜드홀에서 만나요.',
+//   openGraph: {
+//     title: '지훈❤️수아 결혼식에 초대합니다',
+//     description: '2025년 10월 18일 토요일 오후 1시, 서울 라마다호텔 2층 그랜드홀에서 만나요.',
+//     url: 'https://fern-eunsang.vercel.app/',
+//     siteName: '지훈❤️수아 모바일 청첩장',
+//     images: [
+//       {
+//         url: 'https://github.com/ffferncake/mobile-wedding-app/blob/main/public/photo1.PNG', // Replace with actual image URL
+//         width: 800,
+//         height: 600,
+//         alt: '지훈❤️수아 웨딩 사진',
+//       },
+//     ],
+//     type: 'website',
+//   },
+//   twitter: {
+//     card: 'summary_large_image',
+//     title: '지훈❤️수아 결혼식에 초대합니다',
+//     description: '2025년 10월 18일 토요일 오후 1시, 서울 라마다호텔 2층 그랜드홀에서 만나요.',
+//     images: ['https://github.com/ffferncake/mobile-wedding-app/blob/main/public/photo1.PNG'],
+//   },
+// }
 
 export default function WeddingInvitation() {
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>수산♡수이 결혼식에 초대합니다</title>
         <meta property="og:title" content="수산♡수이 결혼식에 초대합니다" />
         <meta
@@ -58,7 +58,7 @@ export default function WeddingInvitation() {
           name="twitter:image"
           content="https://fern-eunsang.vercel.app/images/photo1.png"
         />
-      </Head>
+      </Head> */}
 
       <div className={styles.container}>
         <section className={styles.cover}>
