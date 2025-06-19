338a489a2ee54171c9fd073a15e96ffaa52674bb
ffferncake <fernnichanun@gmail.com>
2025-06-19 09:55:50 +0900

    [test] matadata for kakao,line 등

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import styles from "./WeddingInvitation.module.css";
 import Image from "next/image";
 // import { useInView } from "react-intersection-observer";
@@ -6,6 +5,32 @@ import Image from "next/image";
 // import { motion, useAnimation } from "framer-motion";
 import ScrollSection from "../app/components/ScrollSection"; // Client Component
 import Head from "next/head";
+
+export const metadata = {
+  title: '지훈❤️수아 결혼식에 초대합니다',
+  description: '2025년 10월 18일 토요일 오후 1시, 서울 라마다호텔 2층 그랜드홀에서 만나요.',
+  openGraph: {
+    title: '지훈❤️수아 결혼식에 초대합니다',
+    description: '2025년 10월 18일 토요일 오후 1시, 서울 라마다호텔 2층 그랜드홀에서 만나요.',
+    url: 'https://fern-eunsang.vercel.app/',
+    siteName: '지훈❤️수아 모바일 청첩장',
+    images: [
+      {
+        url: 'https://github.com/ffferncake/mobile-wedding-app/blob/main/public/photo1.PNG', // Replace with actual image URL
+        width: 800,
+        height: 600,
+        alt: '지훈❤️수아 웨딩 사진',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '지훈❤️수아 결혼식에 초대합니다',
+    description: '2025년 10월 18일 토요일 오후 1시, 서울 라마다호텔 2층 그랜드홀에서 만나요.',
+    images: ['https://github.com/ffferncake/mobile-wedding-app/blob/main/public/photo1.PNG'],
+  },
+}
 
 export default function WeddingInvitation() {
   return (
