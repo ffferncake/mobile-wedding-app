7dea91e7091e30ad11e196336e7272649c1b413e
ffferncake <59507098+ffferncake@users.noreply.github.com>
2025-06-23 23:33:21 +0900

    노래,사진,계좌번호,이미지 바꾸기 남았음

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect, useRef } from "react";
 import styles from "./WeddingInvitation.module.css";
 import Image from "next/image";
-import ScrollSection from "../app/components/ScrollSection"; 
+import ScrollSection from "../app/components/ScrollSection";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
@@ -55,16 +55,63 @@ export default function WeddingInvitation() {
     };
   }, []);
 
+  const [timeLeft, setTimeLeft] = useState({
+    days: "00",
+    hours: "00",
+    minutes: "00",
+    seconds: "00"
+  });
+
+  useEffect(() => {
+    const target = new Date("2027-04-10T12:00:00+09:00");
+
+    const update = () => {
+      const now = new Date();
+      const diff = target.getTime() - now.getTime();
+
+      if (diff <= 0) {
+        setTimeLeft({
+          days: "00",
+          hours: "00",
+          minutes: "00",
+          seconds: "00"
+        });
+        return;
+      }
+
+      const days = String(Math.floor(diff / (1000 * 60 * 60 * 24))).padStart(
+        2,
+        "0"
+      );
+      const hours = String(Math.floor((diff / (1000 * 60 * 60)) % 24)).padStart(
+        2,
+        "0"
+      );
+      const minutes = String(Math.floor((diff / (1000 * 60)) % 60)).padStart(
+        2,
+        "0"
+      );
+      const seconds = String(Math.floor((diff / 1000) % 60)).padStart(2, "0");
+
+      setTimeLeft({ days, hours, minutes, seconds });
+    };
+
+    update();
+    const timer = setInterval(update, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <>
       <div className={styles.container}>
         <nav className={styles.tabNav}>
-          <a href="#date">날짜</a>
-          <a href="#quote">연가</a>
+          {/* <a href="#date">날짜</a> */}
+          {/* <a href="#quote">연가</a> */}
           <a href="#message">초대글</a>
+          <a href="#weddinginfo">예식안내</a>
+          <a href="#accountnumber">마음 전하실 곳</a>
           <a href="#location">오시는길</a>
           <a href="#gallery">갤러리</a>
-          <a href="#rsvp">참석</a>
         </nav>
         <h1 className={styles.coverHeader}>결혼일보</h1>
         <section className={styles.cover}>
@@ -72,7 +119,7 @@ export default function WeddingInvitation() {
             유은상{" "}
             <Image
               src="/images/heart_icon_white.png"
-              alt="heart ico white"
+              alt="heart icon white"
               width={32}
               height={32}
             />
@@ -95,18 +142,6 @@ export default function WeddingInvitation() {
           </div>
         </ScrollSection>
 
-        {/* <ScrollSection>
-          <div id="quote" className={styles.quote}>
-            <p>
-              예쁜 연가에 들떴다.
-              <br />
-              우리는 언제나 손을 잡고 있게 될 것이다.
-            </p>
-            <p style={{ fontSize: "12px", marginTop: "5px" }}>
-              《연인》, 이예체
-            </p>
-          </div>
-        </ScrollSection> */}
         <ScrollSection>
           <div id="message" className={styles.inviteMessage}>
             <p className={styles.highlight}>소중한 분들을 초대합니다.</p>
@@ -142,41 +177,73 @@ export default function WeddingInvitation() {
                 </div>
               </div>
             </div>
+
+            <div className={styles.kidsImg}>
+              <Image
+                src="/images/oppa_kids_ver.png"
+                alt="oppa img"
+                width={150}
+                height={150}
+              />{" "}
+              <Image
+                src="/images/me_kids_ver.png"
+                alt="my img"
+                width={150}
+                height={150}
+              />{" "}
+            </div>
           </div>
         </ScrollSection>
 
+        {/* 예식 안내 */}
         <ScrollSection>
-          <div id="location" className={styles.inviteMessage}>
-            <h3 className={styles.highlight}>오시는 길</h3>
+          <div id="weddinginfo" className={styles.inviteMessage}>
+            <h3 className={styles.highlight}>예식 안내</h3>
             <div className={styles.locationInfo}>
-              <p>
-                웨딩시티 신도림 테크노마트 <a href="tel:0221120000">📞</a>
-              </p>
+              <p>2027년 4월 10일 토요일 오후 12시 </p>
+              <p>웨딩시티 신도림 테크노마트</p>
               <p>8층 스타티스홀</p>
-              <p>서울 구로구 새말로 97 신도림테크노마트</p>
             </div>
-            <div ref={mapContainerRef} className={styles.mapContainer} />
-
-            <div className={styles.navLinks}>
-              <a
-                href="https://map.kakao.com/?q=%EC%9B%A8%EB%94%A9%EC%8B%9C%ED%8B%B0%20%EC%8B%A0%EB%8F%84%EB%A6%BC"
-                target="_blank"
-              >
-                카카오내비
-              </a>
-              <a
-                href="https://map.naver.com/p/search/%ED%85%8C%ED%81%AC%EB%85%B8%EB%A7%88%ED%8A%B8%20%EC%9B%A8%EB%94%A9%EC%8B%9C%ED%8B%B0/place/12867934?c=15.00,0,0,0,dh&isCorrectAnswer=true&placePath=/home?from=map&fromPanelNum=1&additionalHeight=76&timestamp=202506222339&locale=ko&svcName=map_pcv5&searchText=%ED%85%8C%ED%81%AC%EB%85%B8%EB%A7%88%ED%8A%B8%20%EC%9B%A8%EB%94%A9%EC%8B%9C%ED%8B%B0"
-                target="_blank"
-              >
-                네이버지도
-              </a>
+            <Image
+              src="/images/weddinginfo_section.png"
+              alt="wedding info section"
+              width={250}
+              height={350}
+            />{" "}
+            {/* ⏱ Countdown Timer */}
+            <div className={styles.flipClock}>
+              <div className={styles.flipUnit}>
+                <div className={styles.flipCard}>
+                  <span>{timeLeft.days}</span>
+                </div>
+                <div className={styles.flipLabel}>DAYS</div>
+              </div>
+              <div className={styles.flipUnit}>
+                <div className={styles.flipCard}>
+                  <span>{timeLeft.hours}</span>
+                </div>
+                <div className={styles.flipLabel}>HOURS</div>
+              </div>
+              <div className={styles.flipUnit}>
+                <div className={styles.flipCard}>
+                  <span>{timeLeft.minutes}</span>
+                </div>
+                <div className={styles.flipLabel}>MINUTES</div>
+              </div>
+              <div className={styles.flipUnit}>
+                <div className={styles.flipCard}>
+                  <span>{timeLeft.seconds}</span>
+                </div>
+                <div className={styles.flipLabel}>SECONDS</div>
+              </div>
             </div>
           </div>
         </ScrollSection>
 
+        {/* 갤러리 */}
         <ScrollSection>
           <div id="gallery" className={styles.inviteMessage}>
-            <h3>갤러리</h3>
+            <h3 className={styles.highlight}>갤러리</h3>
           </div>
 
           <div className={styles.gallery}>
@@ -203,20 +270,75 @@ export default function WeddingInvitation() {
           )}
         </ScrollSection>
 
+        {/* 마음 전하실 곳 */}
         <ScrollSection>
+          <div id="accountnumber" className={styles.inviteMessage}>
+            <h3 className={styles.highlight}>마음 전하실 곳 </h3>
+            <p>
+              소중한 축하를 보내주셔서 감사드리며, <br />
+              따뜻한 마음에 깊이 감사드립니다.
+            </p>
+          </div>
+        </ScrollSection>
+
+        {/* 오시는길 */}
+        <ScrollSection>
+          <div id="location" className={styles.inviteMessage}>
+            <h3 className={styles.highlight}>오시는 길</h3>
+            <div className={styles.locationInfo}>
+              <p>웨딩시티 신도림 테크노마트</p>
+              <p>8층 스타티스홀</p>
+              <p>서울 구로구 새말로 97 신도림테크노마트</p>
+            </div>
+            <div ref={mapContainerRef} className={styles.mapContainer} />
+
+            <div className={styles.mapLinks}>
+              <div className={styles.navLinks}>
+                <Image
+                  src="/images/kakao_navi.svg"
+                  alt="kakao icon"
+                  width={32}
+                  height={32}
+                />
+                <a
+                  href="https://map.kakao.com/?q=%EC%9B%A8%EB%94%A9%EC%8B%9C%ED%8B%B0%20%EC%8B%A0%EB%8F%84%EB%A6%BC"
+                  target="_blank"
+                >
+                  카카오내비
+                </a>
+              </div>
+              <div className={styles.navLinks}>
+                <Image
+                  src="/images/naver_map.png"
+                  alt="naver icon"
+                  width={32}
+                  height={32}
+                />
+                <a
+                  href="https://map.naver.com/p/search/%ED%85%8C%ED%81%AC%EB%85%B8%EB%A7%88%ED%8A%B8%20%EC%9B%A8%EB%94%A9%EC%8B%9C%ED%8B%B0/place/12867934?c=15.00,0,0,0,dh&isCorrectAnswer=true&placePath=/home?from=map&fromPanelNum=1&additionalHeight=76&timestamp=202506222339&locale=ko&svcName=map_pcv5&searchText=%ED%85%8C%ED%81%AC%EB%85%B8%EB%A7%88%ED%8A%B8%20%EC%9B%A8%EB%94%A9%EC%8B%9C%ED%8B%B0"
+                  target="_blank"
+                >
+                  네이버지도
+                </a>
+              </div>
+            </div>
+          </div>
+        </ScrollSection>
+
+        {/* 참석 여부 */}
+        {/* <ScrollSection>
           <div id="rsvp" className={styles.inviteMessage}>
             <h3>참석 여부</h3>
             <a href="https://forms.gle/your-form-url" target="_blank">
               RSVP 하기
             </a>
-            {/* ✅ 카카오 공유 버튼 추가 */}
             <div>
               <button className={styles.shareButton}>
                 💛 카카오톡으로 공유하기
               </button>
             </div>
           </div>
-        </ScrollSection>
+        </ScrollSection> */}
 
         <footer className={styles.footer}>
           © 2025 Eunsang & Fern Wedding Invitation
