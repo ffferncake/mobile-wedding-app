c1347617a0365e7d2a72c8585610ba750494654c
Nichanun Trakulphudphong <fern@Fern-MacBook-Pro.local>
2025-10-11 19:17:34 +0900

    hamburger UI 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,21 @@ import "./globals.css";
 import { ReactNode } from "react";
 
 export const metadata = {
-  title: "은상♡펀 결혼합니다!",
+  title: "은상 ♥ 펀 결혼합니다!",
   description:
     "2027년 04월 10일 토요일 오후 12시, 웨딩시티 신도림 테크노마트 8층 스타티스홀 만나요.",
   openGraph: {
-    title: "은상♡펀 결혼합니다!",
+    title: "은상 ♥ 펀 결혼합니다!",
     description:
       "2027년 04월 10일 토요일 오후 12시, 웨딩시티 신도림 테크노마트 8층 스타티스홀 만나요.",
     url: "https://fern-eunsang.vercel.app/",
-    siteName: "은상♡펀 모바일 청첩장",
+    siteName: "은상 ♥ 펀 모바일 청첩장",
     images: [
       {
         url: "https://github.com/ffferncake/mobile-wedding-app/blob/main/public/photo1.png",
         width: 800,
         height: 600,
-        alt: "은상♡펀 웨딩 사진"
+        alt: "은상 ♥ 펀 웨딩 사진"
       }
     ],
     type: "website"
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="ko">
       <head>
         <link rel="shortcut icon" href="/images/icon/heart_icon.png" />
-        <meta property="og:title" content="은상♡펀 결혼합니다!" />
+        <meta property="og:title" content="은상 ♥ 펀 결혼합니다!" />
         <meta
           property="og:description"
           content="2027년 04월 10일 토요일 오후 12시, 웨딩시티 신도림 테크노마트 8층 스타티스홀 만나요."
